ba109e426e98aa82c514bb7bb7f16fd1738ce81b
Emmanuel-student <emmanuel.esqueda0695@alumnos.udg.mx>
2025-02-19 20:14:23 -0600

    Update Actividad 05 -Juego de Gato.c

--- Actividad 05 -Juego de Gato.c
+++ Actividad 05 -Juego de Gato.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 //Actividad 05 -Juego de Gato
+void menu();
+void inicializar();
+int tablero();
+void verificar();
 int main()
 {
-  printf("Hola mundo\n");
-  printf("Hola\n")
+  menu();
   return 0;
+}
+void menu()
+{
+    int res;
+    printf("Bienvenido Por favor selecciona una opcion\n");
+    printf("1) Jugar\n2) Salir\n");
+    scanf("%d", &res);
+    if(res==1)
+    {
+        inicializar();
+    }
+    else 
+    {
+        printf("Adios\n");
+    }
 }
